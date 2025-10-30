c7a366b3470bea85e81f2e2fe0a91b46c09f7b47
THARUS <2025tharuni@gmail.com>
2025-10-30 23:43:26 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,18 @@
+// Last updated: 10/30/2025, 11:43:40 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+}
+    
+};
+
+
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
