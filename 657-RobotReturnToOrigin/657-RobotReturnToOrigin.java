f2c292055fa5fa23bd7c851247be8f315ee3d1e9
THARUS <2025tharuni@gmail.com>
2026-05-30 08:01:25 +0530

    Create 657-RobotReturnToOrigin

---
+++ 657-RobotReturnToOrigin/657-RobotReturnToOrigin.java
@@ -0,0 +1,17 @@
+// Last updated: 5/30/2026, 8:01:43 AM
+class Solution {
+    public boolean judgeCircle(String moves) {
+        int x = 0, y = 0;
+
+        for(char ch : moves.toCharArray()) {
+            switch(ch) {
+                case 'U': y++; break;
+                case 'D': y--; break;
+                case 'L': x--; break;
+                case 'R': x++; break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
